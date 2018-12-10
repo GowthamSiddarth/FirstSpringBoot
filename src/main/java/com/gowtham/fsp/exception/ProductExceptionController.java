581c90bebc5fea18d312561e7c461abcffcd5e7e
Gowtham Behara <gothsiddu@gmail.com>
2018-12-10 17:50:19 +0530

    Create global exception handler

---
+++ src/main/java/com/gowtham/fsp/exception/ProductExceptionController.java
@@ -0,0 +1,21 @@
+package com.gowtham.fsp.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ProductExceptionController {
+
+    @ExceptionHandler(value = ProductNotFoundException.class)
+    public ResponseEntity<Object> exception(ProductNotFoundException exception) {
+        return new ResponseEntity<>("Product Not Found", HttpStatus.NOT_FOUND);
+    }
+}
+
+package com.gowtham.fsp.exception;
+
+public class ProductNotFoundException extends RuntimeException {
+}
+
