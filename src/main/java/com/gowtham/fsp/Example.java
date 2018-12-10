7c7d29fffa0c206ea502238af7be32a6fc9a127b
Gowtham Behara <gothsiddu@gmail.com>
2018-12-10 13:01:31 +0530

    Extend ServletInitializer and register class for app builder

--- src/main/java/com/gowtham/fsp/Example.java
+++ src/main/java/com/gowtham/fsp/Example.java
@@ -1,3 +1,5 @@
+package com.gowtham.fsp;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
