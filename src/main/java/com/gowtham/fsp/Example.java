935cff2828fd60ee5275145a6b43124bcdb748ed
Gowtham Behara <gothsiddu@gmail.com>
2018-12-10 13:00:59 +0530

    Change packagin to war and add properties

---
+++ src/main/java/com/gowtham/fsp/Example.java
@@ -0,0 +1,25 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class Example extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder applicationBuilder) {
+        return applicationBuilder.sources(Example.class);
+    }
+
+    @RequestMapping(path = "/")
+    String home() {
+        return "Spring Boot";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Example.class, args);
+    }
+}
