88b11c0eb3ad84fc3e593d658d5f0ff21fd468e5
Gowtham Behara <gothsiddu@gmail.com>
2018-12-11 15:19:10 +0530

    Add Servlet request and response filter

---
+++ src/main/java/com/gowtham/fsp/filter/ProductFilter.java
@@ -0,0 +1,26 @@
+package com.gowtham.fsp.filter;
+
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+@Component
+public class ProductFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        System.out.println("Remote Host: " + servletRequest.getRemoteHost());
+        System.out.println("Remote Addr: " + servletRequest.getRemoteAddr());
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
