6193bab93f058b1668bbf10ddc3fd6761f5f2e28
Gowtham Behara <gothsiddu@gmail.com>
2018-12-13 16:03:46 +0530

    Create api to download file with given filenamew

--- src/main/java/com/gowtham/fsp/controller/ProductServiceController.java
+++ src/main/java/com/gowtham/fsp/controller/ProductServiceController.java
@@ -3,6 +3,8 @@ package com.gowtham.fsp.controller;
 import com.gowtham.fsp.exception.ProductAlreadyExistsException;
 import com.gowtham.fsp.exception.ProductNotFoundException;
 import com.gowtham.fsp.model.Product;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -33,7 +36,7 @@ public class ProductServiceController {
     }
 
     @RequestMapping(value = "/products/{id}", method = RequestMethod.DELETE)
-    public ResponseEntity<Object> delete(@PathVariable("id") String id) {
+    public ResponseEntity<Object> deleteProduct(@PathVariable("id") String id) {
         if (!productRepo.containsKey(id)) {
             throw new ProductNotFoundException();
         }
@@ -43,7 +46,7 @@ public class ProductServiceController {
     }
 
     @RequestMapping(value = "/products/{id}", method = RequestMethod.PUT)
-    public ResponseEntity<Object> update(@PathVariable("id") String id, @RequestBody Product product) {
+    public ResponseEntity<Object> updateProduct(@PathVariable("id") String id, @RequestBody Product product) {
         if (!productRepo.containsKey(id)) {
             throw new ProductNotFoundException();
         }
@@ -55,7 +58,7 @@ public class ProductServiceController {
     }
 
     @RequestMapping(value = "/products", method = RequestMethod.POST)
-    public ResponseEntity<Object> create(@RequestBody Product product) {
+    public ResponseEntity<Object> createProduct(@RequestBody Product product) {
         if (productRepo.containsKey(product.getId())) {
             throw new ProductAlreadyExistsException();
         }
@@ -73,12 +76,28 @@ public class ProductServiceController {
     }
 
     @RequestMapping(value = "/products/upload", method = RequestMethod.POST, consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public ResponseEntity<Object> fileUpload(@RequestParam("file") MultipartFile multipartFile) throws IOException {
+    public ResponseEntity<Object> uploadFile(@RequestParam("file") MultipartFile multipartFile) throws IOException {
         File file = new File("D:/IntelliJProjects/fsp/src/main/resources/" + multipartFile.getOriginalFilename());
         file.createNewFile();
         FileOutputStream fileOutputStream = new FileOutputStream(file);
         fileOutputStream.write(multipartFile.getBytes());
         fileOutputStream.close();
         return new ResponseEntity<>("File Uploaded", HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/products/download", method = RequestMethod.POST)
+    public ResponseEntity<Object> downloadFile(@RequestBody Map<String, Object> payload) throws IOException {
+        System.out.println(payload.get("filename"));
+        File file = new File("D:/IntelliJProjects/fsp/src/main/resources/" + payload.get("filename"));
+        InputStreamResource inputStreamResource = new InputStreamResource(new FileInputStream(file));
+
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add("Content-Disposition", String.format("attachment; filename=\"%s\"", payload.get("filename")));
+        httpHeaders.add("Cache-Control", "no-cache, no-store, must-revalidate");
+        httpHeaders.add("Pragma", "no-cache");
+        httpHeaders.add("Expires", "0");
+
+        return ResponseEntity.ok().headers(httpHeaders).contentLength(file.length())
+                .contentType(MediaType.parseMediaType("application/txt")).body(inputStreamResource);
     }
 }
